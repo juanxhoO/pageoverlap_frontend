48e924d2943f9930872632c47e12019e4b38521e
Juan Granja <ggjuanb@hotmail.com>
2022-08-15 12:04:35 -0500

    adding tweaks

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -6,28 +6,8 @@ import React from 'react';
 import { Button } from '@mui/material';
 const Searchbar = () => {
 
-    const top100Films = [
-        { title: 'The Shawshank Redemption', year: 1994 },
-        { title: 'The Godfather', year: 1972 },
-        { title: 'The Godfather: Part II', year: 1974 },
-        { title: 'The Dark Knight', year: 2008 },
-        { title: '12 Angry Men', year: 1957 },
-        { title: "Schindler's List", year: 1993 },
-        { title: 'Pulp Fiction', year: 1994 },
-        {
-            title: 'The Lord of the Rings: The Return of the King',
-            year: 2003,
-        },
-        { title: 'The Good, the Bad and the Ugly', year: 1966 },
-        { title: 'Fight Club', year: 1999 },
-        {
-            title: 'The Lord of the Rings: The Fellowship of the Ring',
-            year: 2001,
-        },
-        {
-            title: 'Star Wars: Episode V - The Empire Strikes Back',
-            year: 1980,
-        }];
+    const search_query_bar = [
+       ];
 
     return (
         <div>
@@ -36,7 +16,7 @@ const Searchbar = () => {
                     freeSolo
                     id="free-solo-2-demo"
                     disableClearable
-                    options={top100Films.map((option) => option.title)}
+                    options={search_query_bar.map((option) => option.title)}
                     renderInput={(params) => (
                         <TextField
                             {...params}
